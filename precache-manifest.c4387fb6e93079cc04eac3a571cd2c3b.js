43ca88ee073967a32d1fcf60897f583d27fbd42f
mshadricheva <shadricheva@mail.ru>
2020-07-10 16:00:44 +0300

    Updates

---
+++ precache-manifest.c4387fb6e93079cc04eac3a571cd2c3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427eb123fa7f2be416c083a757567f59",
+    "url": "/School-timetable-frontend/index.html"
+  },
+  {
+    "revision": "f009856d65893a31d7b6",
+    "url": "/School-timetable-frontend/static/css/main.856e867a.chunk.css"
+  },
+  {
+    "revision": "3dcb0bb6a03b2c8a9819",
+    "url": "/School-timetable-frontend/static/js/2.eac2ec54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/School-timetable-frontend/static/js/2.eac2ec54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f009856d65893a31d7b6",
+    "url": "/School-timetable-frontend/static/js/main.5ab96831.chunk.js"
+  },
+  {
+    "revision": "995238c739a8eb70b2fc",
+    "url": "/School-timetable-frontend/static/js/runtime-main.efc0a02a.js"
+  }
+]);
